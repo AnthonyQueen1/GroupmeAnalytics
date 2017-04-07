128e671440978107e02577a5658045f4f5b276bb
Anthony Queen <anthony.queen.1995@gmail.com>
2017-04-06 23:46:36 -0400

    modified router

--- gserve.js
+++ gserve.js
@@ -7,5 +7,5 @@ var path    = require('path');
 app.use(require('./routes'));
 
 app.listen(port, function() {
-  	console.log('Listening on port 3001...');
+  	console.log('Listening on port ' + port);
 })
--- routes.js
+++ routes.js
@@ -1,37 +1,41 @@
-var express = require('express'), router = express.Router();
+var express = require('express');
+var router  = express.Router();
 var path    = require('path');
 var request = require('request');
 const pug 	= require('pug');
 
 const compiledFunction = pug.compileFile(path.join(__dirname, 'pug/index.pug'));
 
-router.get('/api', function(req, res) {
+router.get('/groupme/api', function(req, res) {
 	// get the token from the url on redirect
-	token = req.url.replace('/api?access_token=', "");
+	var body;
+	token = req.url.replace('/groupme/api?access_token=', "");
 	console.log('access Token: ' + token);
-
-	dispdata();
+	request('https://api.groupme.com/v3/groups?token=' + token, function (error, response, body) {
+		console.log('error:', error);
+		console.log('statusCode:', response && response.statusCode);
+		body = JSON.parse(body);
+	});
 
 	// send user back to homepage after logging in
-  	res.redirect('/pug');
+	res.send(compiledFunction(body));
  });
 
-router.get('/', function(req, res) {
+router.get('/groupme/', function(req, res) {
   	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-router.get('/pug', function(req, res) {
-  	res.send(compiledFunction());
+router.get('/groupme/pug', function(req, res) {
+	body = [1,2.3];
+	res.send(compiledFunction(body));
+});
+
+router.get('*', function(req, res) {
+	res.send('unknown URL in node (groupme): ' + req.url);
 });
 
 function dispdata(){
 	// make request to groupme with new token
-	request('https://api.groupme.com/v3/groups?token=' + token, function (error, response, body) {
-		console.log('error:', error);
-		console.log('statusCode:', response && response.statusCode);
-		// console.log('body:', body);
-		return body;
-	});
 }
 
 module.exports = router;
